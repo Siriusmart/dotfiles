c101eca5fef9e14842dd58b4b348c7d22014f9b9
Siriusmart <sirius@siri.ws>
2026-03-27 10:21:17 +0000

    hyprland dispatchers

--- eggs/nvim/lua/plugins/lean.lua
+++ eggs/nvim/lua/plugins/lean.lua
@@ -1,23 +1,28 @@
 return {
-  'Julian/lean.nvim',
-  event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+    'Julian/lean.nvim',
+    event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
 
-  dependencies = {
-    'nvim-lua/plenary.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
 
-    -- optional dependencies:
+        -- optional dependencies:
 
-    -- a completion engine
-    --    hrsh7th/nvim-cmp or Saghen/blink.cmp are popular choices
+        -- a completion engine
+        --    hrsh7th/nvim-cmp or Saghen/blink.cmp are popular choices
 
-    -- 'nvim-telescope/telescope.nvim', -- for 2 Lean-specific pickers
-    -- 'andymass/vim-matchup',          -- for enhanced % motion behavior
-    -- 'andrewradev/switch.vim',        -- for switch support
-    -- 'tomtom/tcomment_vim',           -- for commenting
-  },
+        -- 'nvim-telescope/telescope.nvim', -- for 2 Lean-specific pickers
+        -- 'andymass/vim-matchup',          -- for enhanced % motion behavior
+        -- 'andrewradev/switch.vim',        -- for switch support
+        -- 'tomtom/tcomment_vim',           -- for commenting
+    },
 
-  ---@type lean.Config
-  opts = { -- see below for full configuration options
-    mappings = true,
-  }
+    ---@type lean.Config
+    opts = { -- see below for full configuration options
+        mappings = true,
+        leanls = {
+            init_options = {
+                editDelay = 100,
+            }
+        },
+    }
 }
